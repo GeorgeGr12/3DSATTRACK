8e2b207d8d195c16b47271f0688d11a4cf1c9935
GeorgeGr12 <georgegrammatosis@gmail.com>
2025-10-24 23:44:50 -0400

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,15 @@
 // Main initialization
 
 function init() {
-    // Initialize the 3D scene
     initScene();
     
-    // Start date/time updates
     updateDateTime();
     setInterval(updateDateTime, 1000);
     
-    // Start satellite position updates
     console.log('Starting satellite position updates...');
     updateAllSatellites();
     setInterval(updateAllSatellites, UPDATE_INTERVAL);
     
-    // Start animation loop
     animate();
 }
 
